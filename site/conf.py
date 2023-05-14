5fdfeb7295cb9be67eeba50d7b2cbb39005f7714
Dugal Harris <dugalh@gmail.com>
2023-05-14 17:39:56 +0200

    tidy to-do's

--- site/conf.py
+++ site/conf.py
@@ -91,6 +91,7 @@ html_theme_options = {
     'collapse_navigation': True,
     'show_nav_level': 0,
     'navigation_depth': 0,
+    # "announcement": "<p>Here's a <a href='https://pydata.org'>PyData Announcement!</a></p>",
 }
 
 # -- favicon extension optionse  ------------------------------------------
@@ -129,60 +130,4 @@ with dest_path.open(mode='wt') as dest_file:
 # TODO: auto generate ico
 # TODO: also sort out sponsorship to me or organisation
 # TODO: add a license for data / images
-# TODO: IE does not use gap setting from theme flow, so requires css for .nav-link to add padding.  But then new browsers
-#  add this padding to the flex gap.  Ideally I should either get IE to use a gap specific to it.  Or zero the gap, and
-#  use only padding for all browsers.  This is in navbar-item->navbar-nav.
-#  The above applies to section headers and breadcrumbs.
-# TODO: Safari is rendering vertically what newer browsers render horizontally e.g. the header section titles and breadcrumbs.
-# x TODO: Safari and IE announcement banner coloring
-# x TODO: IE does not re-configure layout when it is resized
-# x TODO: Safari and IE put underline with header / sidebar hover
-# x TODO: browserstack safari 5 does not have the breadcrumb triangle
-# TODO: safari 5 sidebar does not go away if you click outside of it, only inside
-# TODO: safari renders grid as though it is on low res i.e. one column
 # TODO: think about including portfolio items in drop-down or sidebar sub-menu
-# TODO: could we auto-generate css with all the var(...)'s evaluated?  Then we wouldn't need all the custom coloring etc code.
-#  The problem is it is dynamic i.e. it needs to know the theme at browse time to determine the val of e.g. --pst-*-color
-# TODO: change grid layouts for smart phone side view (?)  bear in mind this could expose display: flex issues.
-# \\127.0.0.1\c$\Data\Development\Projects\leftfield.online\site\_build\html\index.html
-# TODO: note safari seems to start supporting flex properly in macos v 9, but it renders nothing other than the nav bar / header. 
-#  Likewise for v10, then it renders in v11 (something is weird with spacing as sidebar sections are on top of each other).
-#  webp support only starts in v14 (2020).
-#  For iphone, the site looks ok as far back as iphone 5s&6, but safari does not seem to support css var as colors are wrong and no grid borders.
-#  The sidebar works and grids display ok though.  Webp support starts round about iphone 8.
-#  On Mac the sidebar gets stuck like mine on safari v5, then from v6 is ok.
-# TODO: On IE 9, logo is elongated vertically and cropped, there is no webp message, and as header section titles are not on RHS.
-#  flex grids render vertically.
-#  On IE 10, (windows 7 & 8) everything is as above except that grids start rendering horizontally where they should.  Also the sidebar is transparent.
-#  On IE 11, (windows 7 & 8), weirdly the logo is stretched as above.  The rest looks as it does on my local IE 11.
-#  On IE 11, (windows 10), the logo displays ok, and the rest is as it is on my pc.
-# TODO: Edge is fine back to its first version on win 10.
-#  The first version of Edge does not support webp or dark/light themes.  It supports flex and var ok, and the rest is ok.
-#  Starting with v18 (2018) it supports webp and dark/light themes.
-
-
-# TODO: sidebar 0.13.3:
-# TODO:
-#   // header:
-#   bd-header / navbar / bd-navbar
-#       ...
-#       navbar-item
-#           logo
-#       navbar-header-items
-#           navbar-item
-#               navbar-nav (bd-navbar-elements)
-#                   nav-item
-#                       nav-link
-#                           (Portfolio/Profile/Contact)
-#               theme switcher
-#               navbar-icon-links
-#   // sidebar:
-#   bd-sidebar-primary bd-sidebar hide-on-wide
-#       sidebar-header-items
-#           navbar-item
-#               navbar-nav
-#                   nav-item
-#                           (Portfolio/Profile/Contact)
-#               theme switcher
-#               navbar-icon-links
-
